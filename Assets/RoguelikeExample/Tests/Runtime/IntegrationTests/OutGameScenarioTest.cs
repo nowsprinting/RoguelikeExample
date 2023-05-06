371f7e31b9b9c65e8558842f4e53627c039af81a
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-05-06 14:40:47 +0900

    Fix button names

--- Assets/RoguelikeExample/Tests/Runtime/IntegrationTests/OutGameScenarioTest.cs
+++ Assets/RoguelikeExample/Tests/Runtime/IntegrationTests/OutGameScenarioTest.cs
@@ -14,14 +14,15 @@ namespace RoguelikeExample.IntegrationTests
     /// アウトゲームのシナリオテスト（統合テスト）
     ///
     /// Automated QAパッケージのRecorded Playback機能を使用して、あらかじめ記録した操作を再生しています。
-    /// 再生完了後、インゲームに遷移していることを検証しています。
+    /// Test Generation機能で *Simple Test* を選択して生成されるコードをベースにしています。
+    /// 再生完了後、インゲームに遷移していることを検証するアサーションを追加しています。
     /// </summary>
     [TestFixture]
     [Category("Integration")]
     public class OutGameScenarioTest : RecordedTestSuite
     {
         [UnityTest]
-        [RecordedTest("RoguelikeExample/Tests/TestData/AutomatedQA/recording.json")]
+        [RecordedTest("RoguelikeExample/Tests/TestData/AutomatedQA/recording-all.json")]
         public IEnumerator アウトゲームのシナリオテスト_AQA記録ファイルを再生_タイトル画面を一通り遷移してインゲームに遷移()
         {
             yield return RecordedTesting.TestPlayToEnd();
